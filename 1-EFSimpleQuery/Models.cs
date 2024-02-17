4b5f45f7f78e3f108a201bffee8294d4ed6fed46
rafael <rafaondjango@gmail.com>
2024-02-17 15:49:34 -0300

    Adicionar arquivos de projeto.

---
+++ 1-EFSimpleQuery/Models.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFSimpleQuery.Models;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set;}
+    public DbSet<Post> Posts { get; set;}
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source= {DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; set; } = [];
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
---
+++ 1-EFSimpleQuery/Program.cs
@@ -0,0 +1,33 @@
+﻿using EFSimpleQuery.Models;
+using Microsoft.EntityFrameworkCore;
+
+using var db = new BloggingContext();
+
+Console.WriteLine(db.DbPath);
+
+// Inserting data
+Console.WriteLine("Adicionando um novo blog");
+
+db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+db.SaveChanges();
+
+// Fetching data
+Console.WriteLine("Buscando um blog no banco de dados");
+
+var blog = db.Blogs.OrderBy(blog => blog.BlogId).FirstOrDefault();
+
+//Updating data
+Console.WriteLine("Atualizando um blog");
+
+blog.Url = "https://www.google.com.br";
+blog.Posts.Add(
+    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" }
+);
+
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Deletando um blog");
+db.Remove(blog);
+db.SaveChanges();
+
